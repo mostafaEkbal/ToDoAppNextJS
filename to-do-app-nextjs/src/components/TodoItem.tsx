2861094f3f71fcfe4eef8c72be6890770aaadc7d
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2024-06-03 15:30:25 +0300

    toggle todo functionality

--- to-do-app-nextjs/src/components/TodoItem.tsx
+++ to-do-app-nextjs/src/components/TodoItem.tsx
@@ -1,17 +1,29 @@
-type TodoItemProps = {
-    id: string
-    title: string
-    complete: Boolean
-}
+'use client';
 
-export function TodoItem({ id, title, complete }: TodoItemProps) {
-    return (
-        <li className="flex gap- items-center">
-            <input type="checkbox" id={id} className="cursor-pointer peer" />
-            <label htmlFor={id} className="peer-checked:line-through cursor-pointer
-            peer-checked:text-slate-500">
-                {title}
-            </label>
-        </li>
-    )
+type TodoItemProps = {
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+};
+
+export function TodoItem({id, title, complete, toggleTodo}: TodoItemProps) {
+  return (
+    <li className='flex gap- items-center'>
+      <input
+        type='checkbox'
+        id={id}
+        className='cursor-pointer peer'
+        defaultChecked={complete}
+        onChange={e => toggleTodo(id, e.target.checked)}
+      />
+      <label
+        htmlFor={id}
+        className='peer-checked:line-through cursor-pointer
+            peer-checked:text-slate-500'
+      >
+        {title}
+      </label>
+    </li>
+  );
 }
