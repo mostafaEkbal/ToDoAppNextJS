08129c7091fdb79e576fd5b356b86bef59675603
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2024-06-05 09:29:10 +0300

    invalid title handle

--- to-do-app-nextjs/src/app/new/page.tsx
+++ to-do-app-nextjs/src/app/new/page.tsx
@@ -7,7 +7,7 @@ async function createTodo(data: FormData) {
 
   const title = data.get('title')?.valueOf();
   if (typeof title !== 'string' || title.length === 0) {
-    throw new Error('Invalid Title');
+    return;
   }
 
   await prisma.todo.create({data: {title, complete: false}});
